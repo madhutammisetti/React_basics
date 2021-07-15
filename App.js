094534bdb2ef375b4d1cbfe564aa68ec6cd0acfd
madhutammisetti <85747431+madhutammisetti@users.noreply.github.com>
2021-07-15 15:46:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./style.css"
+import  Button from "./Button.js"
+const Madhu=()=>{                                       // arrow function
+    return(
+        <div>
+        <h1 style ={{textAlign:"center"}}> Namasthe React </h1>
+        <h2>Hello Youtubers</h2>
+        <Button buttonText="Submit"/>
+        <Button buttonText="ClickHere"/>
+        <Button buttonText="Download"/>
+       
+        </div>
+    )
+}
+export default Madhu
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Madhu from "./App.js"
+
+
+ReactDOM.render(<Madhu/>, document.getElementById("root"))  
